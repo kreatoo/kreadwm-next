fef340d52848918b4ba1c4be06ba401f7aab710a
Kreato <kreato@kreatea.space>
2021-08-17 21:54:23 +0300

    a

---
+++ patch/include.h
@@ -0,0 +1,7 @@
+/* Bar functionality */
+#include "bar_ltsymbol.h"
+//#include "bar_status.h"
+#include "bar_tags.h"
+#include "bar_wintitle.h"
+//#include "bar_powerline_tags.h"
+#include "bar_powerline_status.h"
